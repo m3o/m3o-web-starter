dd1c6f483106743271cb11112439afc2d701990a
Martin Virando <martinvirando@hotmail.co.uk>
2021-11-30 12:35:25 +0000

    Added a protected client view

--- pages/protected-server.tsx
+++ pages/protected-server.tsx
@@ -5,7 +5,8 @@ import { withAuth } from '@m3o/nextjs'
  * This is an example of the route being blocked by the server.
  * This is generally slower and means that you lose Static Generation.
  * The user prop will be picked up within the `_app.tsx` file and passed automatically
- * to the <UserProvider />. This saves passing the provider on each page.
+ * to the <UserProvider />. This saves passing the provider on each page. For more information see
+ * https://nextjs.org/docs/authentication
  */
 export const getServerSideProps = withAuth(async context => {
   if (!context.req.user) {
